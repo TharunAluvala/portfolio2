a231dbe625b3fd88d555c6c167a550e9ae0098da
Aluvala Nithin Goud <anithingoud2001@gmail.com>
2024-07-27 04:41:11 -0700

    first commit

---
+++ src/sections/Contact/Contact.jsx
@@ -0,0 +1,70 @@
+import React, { useRef } from 'react';
+import styles from './ContactStyles.module.css';
+import emailjs from '@emailjs/browser';
+
+function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_z26ot39', 'template_mbh9nvk', form.current, '9K5z2EyQTYB2OhSQ2')
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          alert('Details sent successfully!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+          alert('Failed to send details.');
+        }
+      );
+  };
+
+  return (
+    <section id="contact" className={styles.container}>
+      <h1 className="sectionTitle">Contact</h1>
+      <form ref={form} onSubmit={sendEmail}>
+        <div className="formGroup">
+          <label htmlFor="name" hidden>
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            required
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="email" hidden>
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            required
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="message" hidden>
+            Message
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Message"
+            required
+          ></textarea>
+        </div>
+        <input className="hover btn" type="submit" value="Submit" />
+      </form>
+    </section>
+  );
+}
+
+export default Contact;
---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,43 @@
+import styles from './ProjectsStyles.module.css';
+import viberr from '../../assets/viberr.png';
+import freshBurger from '../../assets/fresh-burger.png';
+import hipsster from '../../assets/hipsster.png';
+import fitLift from '../../assets/fitlift.png';
+import ProjectCard from '../../common/ProjectCard';
+// import Dimage from './public/dimage.png';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src='public/ximage.png'
+          link="https://dappsocial.io/crossx/offers/"
+          h3="Cross-X"
+          p="Decentralized Cross-Chain"
+        />
+        <ProjectCard
+          src='public/dimage.png'
+          link="https://dappsocial.io/vault/"
+          h3="DVault"
+          p="Secure Personnel Locker"
+        />
+        <ProjectCard
+          src='public/dimage.png'
+          link="https://risestreet.io/dstreet/"
+          h3="DStreet"
+          p="A large NFT Marketplace"
+        />
+        {/* <ProjectCard
+          src={fitLift}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="FitLift"
+          p="Fitness App"
+        /> */}
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
